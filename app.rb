c9a220748f6c4bba331fc83eba3d9ad8f858693e
CodeKrakken <newholm1@hotmail.com>
2020-02-27 18:00:20 +0000

    Add welcome message

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ set :session_secret, 'super secret'
 class RubyCats < Sinatra::Base
 
   get '/' do
-    "Hello World!"
+    "Greetings my friend. Care for a cat?"
   end
 
 end
--- spec/features/test_test_spec.rb
+++ spec/features/test_test_spec.rb
@@ -1,6 +1,6 @@
 feature 'Testing Infrastructure' do
   scenario 'Can run app and see page content' do
     visit('/')
-    expect(page).to have_content "Hello World!"
+    expect(page).to have_content "Greetings my friend. Care for a cat?"
   end
 end
